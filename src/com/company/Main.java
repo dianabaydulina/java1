efe4630dd51c14f094762c85cf717276b0910e99
Диана Байдулина <73581596+dianabaydulina@users.noreply.github.com>
2020-10-30 14:49:46 +0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -26,25 +26,26 @@ public class Main {
 
     }
 }
-//package com.company;
-//
-//import java.util.Scanner;
-//
-//public class Main {
-//
-//    public static void main(String[] args) {
-//	float a,b;
-//        Scanner in = new  Scanner(System.in);
-//        a = in.nextFloat();
-//        b = in.nextFloat();
-//
-//        if (a>b)
-//        {
-//            System.out.println(a);
-//        }
-//        else{
-//            System.out.println(b);
-//        }
-//
-//    }
-// }
+/*package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float a,b;
+        Scanner in = new  Scanner(System.in);
+        a = in.nextFloat();
+        b = in.nextFloat();
+
+        if (a>b)
+        {
+            System.out.println(a);
+        }
+        else{
+            System.out.println(b);
+        }
+
+    }
+
+ */
