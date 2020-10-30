03535377eab40c0bcd94db030e9ade3ff907c885
dianabaydulina <73581596+dianabaydulina@users.noreply.github.com>
2020-10-30 14:41:36 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,50 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	float a,b;
+        Scanner in = new  Scanner(System.in);
+        System.out.println("Введите число a: ");
+        a = in.nextFloat();
+        System.out.println("Введите число b: ");
+        b = in.nextFloat();
+
+        if (a>b)
+        {
+            System.out.println("Число " + a + " Больше");
+        }
+        else if (a<b)
+        {
+            System.out.println("Число " + b + " Больше");
+        }
+        else {
+            System.out.println("Числа равны");
+        }
+
+    }
+}
+//package com.company;
+//
+//import java.util.Scanner;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//	float a,b;
+//        Scanner in = new  Scanner(System.in);
+//        a = in.nextFloat();
+//        b = in.nextFloat();
+//
+//        if (a>b)
+//        {
+//            System.out.println(a);
+//        }
+//        else{
+//            System.out.println(b);
+//        }
+//
+//    }
+// }
